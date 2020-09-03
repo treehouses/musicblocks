8a5c3becb24892c3c340c65d69e88851259732fc
Anna X <vmnet8@gmail.com>
2020-09-02 21:39:46 -0700

    add deploy.sh

--- docker-build.sh
+++ docker-build.sh
@@ -3,8 +3,8 @@
 source sha_function.sh
 
 if [ $# -le 1 ]; then
-    echo "missing parameters."
-    exit 1
+   echo "missing parameters."
+   exit 1
 fi
 
 sha=$(get_manifest_sha $@)       # $1 treehouses/nginx:latest  amd64|arm|arm64
@@ -14,12 +14,11 @@ echo $base_image
 arch=$2   # arm arm64 amd64
 
 if [ -n "$sha" ]; then
-        tag=treehouses/musicblocks-tags:$arch
+   tag=treehouses/musicblocks-tags:$arch
         #sed "s|{{base_image}}|$base_image|g" Dockerfile.template > /tmp/Dockerfile.$arch
-        sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
+   sed "s|{{base_image}}|$base_image|g" Dockerfile.template > Dockerfile.$arch
         #cat /tmp/Dockerfile.$arch
-        docker build -t $tag -f Dockerfile.$arch .
+   docker build -t $tag -f Dockerfile.$arch .
        # version=$(docker run -it $tag /bin/sh -c "nginx -v" |awk '{print$3}')
        # echo "$arch nginx version is $version"
-        docker push $tag
 fi
